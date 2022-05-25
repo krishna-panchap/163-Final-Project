cc490a7c1153c95899072b44e3d90e27ac17a65f
markpock <markpock7@gmail.com>
2022-05-25 16:31:15 -0700

    Template file added

---
+++ template.py
@@ -0,0 +1,23 @@
+"""
+Krishna Panchapagesan, Mark Pock
+CSE 163 Final Project
+
+Insert documentation here.
+"""
+
+
+from typing import Iterable
+
+
+def template(**kwargs: Iterable[object]) -> object:
+    """
+    Insert documentation here.
+    """
+    pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
