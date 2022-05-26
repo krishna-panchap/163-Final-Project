89d230522d437257c9c7772e00de7ef4acc9ab70
markpock <markpock7@gmail.com>
2022-05-26 16:20:19 -0700

    also created a utils file w/ time series func

--- utils.py
+++ utils.py
@@ -1,13 +1,18 @@
 import pandas as pd
 
 
-def time_series(filename: str, lib: str = '') -> pd.DataFrame:
+def time_series(filename: str, lib: str = '',
+                concat: bool = False) -> pd.DataFrame:
     """
-    Transforms a single csv representing historical data for a stock given by a
-    str filename into a Time Series, returning the DataFrame.
+    Transforms a csv from the datasets folder into a Time Series as a pandas
+    dataframe. The filename will be the name of the file alone without the
+    csv extension, the library will be the internal folder (e.g. 'stocks/raw/')
+    and concat represents whether or not to associate the names of the columns
+    with the file.
     """
     path = './datasets/' + lib + filename + '.csv'
     df = pd.read_csv(path, index_col='Date', parse_dates=True)
     df = df.loc[:, df.columns != 'Unnamed: 0']
-    df.columns = [(filename + ' ' + col) for col in df.columns]
+    if concat:
+        df.columns = [(filename + ' ' + col) for col in df.columns]
     return df
