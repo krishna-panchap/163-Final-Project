3f9a160466456af087e418b1846af3cd0a6a9ddf
Krishna Panchap <kpanchap@uw.edu>
2022-05-26 17:04:06 -0700

    create updated list of war

--- scrape_war_wiki.py
+++ scrape_war_wiki.py
@@ -11,18 +11,51 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def main():
-    # getting request from
-    wiki_url = 'https://en.wikipedia.org/wiki/List_of_conflicts_by_duration'
-    response = requests.get(wiki_url)
-
-    # parsing the file using bs4
+def extract_data_frame(url):
+    '''
+    this helper function takes in a wiki url and returns a dataframe containing
+    information about the tables on the wiki
+    '''
+    response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')  # soup setup
     table = soup.find_all('table')  # finds all occurences of a soup object
-    war_history = str(table[2])  # selects the right table
-    listwar = pd.read_html(war_history)  # extracts list from html table
-    wartable = pd.DataFrame(listwar[0])  # converts list to data frame
-    wartable.to_csv('list_of_wars.csv')
+    df_war = pd.concat(
+                      [df[0] for df in [pd.read_html(str(table[i]))
+                       for i in range(len(table) - 1)]]
+                    )
+    return df_war
+
+
+def get_old_data(urls):
+    '''
+    extracts the dataframes from each of the inputted list of urls
+    '''
+    list_of_dfs = []
+    for url in urls:
+        list_of_dfs.append(extract_data_frame(url))
+    return pd.concat(list_of_dfs)
+
+
+def get_present_data(url):
+    '''
+    this function returns the dataframe with the tables in the given url
+    '''
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    war_history = soup.find('table', {'class': 'wikitable sortable'})
+    return pd.DataFrame(pd.read_html(str(war_history))[0])
+
+
+def main():
+    url_1900_1944 = 'https://en.wikipedia.org/wiki/List_of_wars:_1900-1944'
+    url_1945_1989 = 'https://en.wikipedia.org/wiki/List_of_wars:_1945-1989'
+    url_1990_2002 = 'https://en.wikipedia.org/wiki/List_of_wars:_1990-2002'
+    urls = [url_1900_1944, url_1945_1989, url_1990_2002]
+    new_url = 'https://en.wikipedia.org/wiki/List_of_wars:_2003-present'
+    old_df = get_old_data(urls)
+    new_df = get_present_data(new_url)
+    combined_df = old_df.append(new_df)
+    combined_df.to_csv('updated_list_of_wars.csv')
 
 
 if __name__ == '__main__':
